b16bc80c57531c3c0c6f64adc4860396a9defc18
David_Creat <80436092+DavidCreat@users.noreply.github.com>
2023-09-17 12:09:35 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'template full by: eas1_music'
+
+files {
+    'data/handling.meta',
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/carcols.meta',
+    'data/dlctext.meta',
+    'data/contentunlocks.meta',
+    'data/vehiclelayouts.meta',
+    'data/shop_vehicle.meta',
+    'data/caraddoncontentunlocks.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/dlctext.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
